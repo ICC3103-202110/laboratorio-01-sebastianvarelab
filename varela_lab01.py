31c806fe35a2097aae38e589cfa253048d53b7d1
sebastianvarelab <svarela@miuandes.cl>
2021-03-25 23:21:26 -0300

    varela_lab1.33

--- varela_lab01.py
+++ varela_lab01.py
@@ -51,7 +51,7 @@ while board_1==board_2:
     coordinate_2_x=int(input("choose your second card coordinate x "))
     while len(board_1)<=coordinate_2_x:
         print("your x coordinate doesn´t exists")
-        coordinate_1_x=int(input("choose your second card coordinate x "))
+        coordinate_2_x=int(input("choose your second card coordinate x "))
     coordinate_2_y=int(input("choose your second card coordinate y "))
     while 2<=coordinate_2_y:
         print("your y coordinate doesn´t exists, remember that y can only be 0 or 1")
@@ -66,3 +66,101 @@ while board_1==board_2:
         board_2[coordinate_2_y]=p_cards_2[coordinate_2_y]
     print(board_1)
     print(board_2)
+    search_p_c=[]
+    help_issue=len(board_1)-1
+    help_issue2=len(board_2)-1
+    while help_issue!=-1:
+        if board_1[help_issue]!="*":
+            search_p_c.append(board_1[help_issue])
+        help_issue-=1
+    while help_issue2!=-1:
+        if board_2[help_issue2]!="*":
+            search_p_c.append(board_2[help_issue2])
+        help_issue2-=1
+    if search_p_c[0]==search_p_c[1]:
+        #pj1+=1
+        board_1.remove(search_p_c[0])
+        board_2.remove(search_p_c[0])
+        print(points,"points")
+    else:
+        help_issue3=len(board_1)-1
+        help_issue4=len(board_2)-1
+        while help_issue3!=-1:
+            if board_1[help_issue3]!="*":
+                board_1[help_issue3]="*"
+            help_issue3-=1
+        while help_issue4!=0:
+            if board_2[help_issue4]!="*":
+                board_2[help_issue4]="*"
+            help_issue4-=1
+###########################################################################################
+        print(points,"points")
+        print("player 2 is your turn to play")
+    coordinate_1_x=int(input("choose your first card coordinate x "))
+    while len(board_1)<=coordinate_1_x:
+        print("your x coordinate doesn´t exists")
+        coordinate_1_x=int(input("choose your first card coordinate x "))
+    coordinate_1_y=int(input("choose your first card coordinate y "))
+    while 2<=coordinate_1_y:
+        print("your y coordinate doesn´t exists, remember that y can only be 0 or 1")
+        coordinate_1_y=int(input("choose your first card coordinate y "))
+    lista=[]
+    lista.append(coordinate_1_x)
+    lista.append(coordinate_1_y)
+    print(lista, "this is the coordinate that you choose")
+    if coordinate_1_y==0:
+        board_1[coordinate_1_x]=p_cards_1[coordinate_1_x]
+    else:
+        board_2[coordinate_1_y]=p_cards_2[coordinate_1_y]
+    coordinate_2_x=int(input("choose your second card coordinate x "))
+    while len(board_1)<=coordinate_2_x:
+        print("your x coordinate doesn´t exists")
+        coordinate_2_x=int(input("choose your second card coordinate x "))
+    coordinate_2_y=int(input("choose your second card coordinate y "))
+    while 2<=coordinate_2_y:
+        print("your y coordinate doesn´t exists, remember that y can only be 0 or 1")
+        coordinate_2_y=int(input("choose your second card coordinate y "))
+    lista=[]
+    lista.append(coordinate_2_x)
+    lista.append(coordinate_2_y)
+    print(lista, "this is the coordinate that you choose")
+    if coordinate_2_y==0:
+        board_1[coordinate_2_x]=p_cards_1[coordinate_2_x]
+    else:
+        board_2[coordinate_2_y]=p_cards_2[coordinate_2_y]
+    print(board_1)
+    print(board_2)
+    search_p_c=[]
+    help_issue=len(board_1)-1
+    help_issue2=len(board_2)-1
+    while help_issue!=-1:
+        if board_1[help_issue]!="*":
+            search_p_c.append(board_1[help_issue])
+        help_issue-=1
+    while help_issue2!=-1:
+        if board_2[help_issue2]!="*":
+            search_p_c.append(board_2[help_issue2])
+        help_issue2-=1
+    if search_p_c[0]==search_p_c[1]:
+        #pj2+=1
+        board_1.remove(search_p_c[0])
+        board_2.remove(search_p_c[0])
+        print(points,"points")
+    else:
+        help_issue3=len(board_1)-1
+        help_issue4=len(board_2)-1
+        while help_issue3!=-1:
+            if board_1[help_issue3]!="*":
+                board_1[help_issue3]="*"
+            help_issue3-=1
+        while help_issue4!=0:
+            if board_2[help_issue4]!="*":
+                board_2[help_issue4]="*"
+            help_issue4-=1
+        print(points,"points")
+        print("player 1 is your turn to play")
+        
+            
+        
+print(board_1)
+print(board_2)
